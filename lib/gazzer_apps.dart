fe463a3c62f61a289e9252342be1a098d1da1fbd
mohammed nashat <mnasha.dev@gmail.com>
2026-09-12 12:34:57 +0300

    Refactor model exports and create models.dart for better organization

--- lib/gazzer_apps.dart
+++ lib/gazzer_apps.dart
@@ -1,6 +1,6 @@
 library gazzer_apps;
 
-export 'src/models/new_order_item_variation.dart';
+export 'src/models/models.dart';
 export 'src/ui/variation_tree.dart';
 export 'src/ui/qty_badge.dart';
 export 'src/enums/order_status.dart';
---
+++ lib/src/models/models.dart
@@ -0,0 +1,3 @@
+
+export 'new_order_item_variation.dart';
+export 'order_history.dart';
